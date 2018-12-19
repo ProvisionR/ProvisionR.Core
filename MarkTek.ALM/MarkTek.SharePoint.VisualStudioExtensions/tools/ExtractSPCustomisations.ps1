88536b2fb72239d062dc22fe246abfb5f39f82b5
Mark Cunningham <mark.cunningham@capgemini.com>
2018-12-19 12:20:20 +0000

    Added Script to tools

---
+++ MarkTek.ALM/MarkTek.SharePoint.VisualStudioExtensions/tools/ExtractSPCustomisations.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()] 
+param
+(
+	[parameter(Mandatory=$true)]
+	[string]$solutionFilesFolder, #The folder to extract the SharePoint Customisations
+	[parameter(Mandatory=$true)]
+	[string]$connectionString, #The SharePoint site Url,
+	[parameter(Mandatory=$true)]
+	[string]$username,
+	[parameter(Mandatory=$true)]
+	[string]$password
+)
+
+Set-ExecutionPolicy –ExecutionPolicy RemoteSigned –Scope CurrentUser
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$moduleName = $scriptPath + "\MarkTek.Devops.SharePoint.PowerShellModule.dll"        
+
+Write-Output "Importing: $moduleName" 
+
+Import-Module -Name $moduleName   
+
+Export-Customisations -SharePointSiteUrl $connectionString -SharePointUserName $username -SharePointPassword $password -ExportPath $solutionFilesFolder
